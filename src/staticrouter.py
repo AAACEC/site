63bc4ab87dba867fa46e8d7f956051f6e3f4f3ab
Gabriela Neri <gabrielapneri@gmail.com>
2017-01-28 18:50:39 -0200

    Starting products page

--- src/staticrouter.py
+++ src/staticrouter.py
@@ -1,8 +1,8 @@
-from bottle import Bottle, static_file, response
+from bottle import Bottle, static_file, run, response
 
 app = Bottle()
 
 @app.route('/<pagename>')
 def get_page(pagename):
     pagename += ".html"
-    return static_file(pagename, root="static/")
+    return static_file(pagename, root='static/')
